cf43d70b59315e53a33bb3cd770afc21d348e0d3
MMOSimca <MMOSimca@users.noreply.github.com>
2017-05-25 19:24:33 -0400

    Added QuestData for quests: Bleeding the Bleeding Hollow (38040), Dirge of the Dead (41024), Battery Backup (46750), and Mission: Felrage Destruction (46756).

--- LibObjectiveProgress-1.0.lua
+++ LibObjectiveProgress-1.0.lua
@@ -5,7 +5,7 @@ Developed by: Simca@Malfurion (MMOSimca)
 
 -- Set major/minor version
 -- Ideally, would use '@project-revision@' here, but it is broken in embeds, so manually incrementing this for now
-local MAJOR, MINOR = "LibObjectiveProgress-1.0", 16
+local MAJOR, MINOR = "LibObjectiveProgress-1.0", 17
 assert(LibStub, MAJOR .. " requires LibStub")
 
 -- Initialize library
