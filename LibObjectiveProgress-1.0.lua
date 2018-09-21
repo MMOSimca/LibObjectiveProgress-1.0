c20375e71590c2030cb0e99d8365a693b7f8ce4e
Caleb Atherton <atcaleb@twitch.tv>
2018-09-21 02:59:40 -0400

    Readded a few quests accidentally removed in the last quest data update.

--- LibObjectiveProgress-1.0.lua
+++ LibObjectiveProgress-1.0.lua
@@ -4,7 +4,7 @@ Developed by: Simca@Malfurion (MMOSimca)
 ]]--
 
 -- Set major/minor version
-local MAJOR, MINOR = "LibObjectiveProgress-1.0", 35
+local MAJOR, MINOR = "LibObjectiveProgress-1.0", 36
 assert(LibStub, MAJOR .. " requires LibStub")
 
 -- Initialize library
