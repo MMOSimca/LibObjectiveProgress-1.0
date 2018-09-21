a26f7eeaceaa336aed4bb730466b9ad99a8882d2
Caleb Atherton <atcaleb@twitch.tv>
2018-09-21 02:35:45 -0400

    Updated quest data with more quests from Patch 8.0.1.

--- LibObjectiveProgress-1.0.lua
+++ LibObjectiveProgress-1.0.lua
@@ -4,7 +4,7 @@ Developed by: Simca@Malfurion (MMOSimca)
 ]]--
 
 -- Set major/minor version
-local MAJOR, MINOR = "LibObjectiveProgress-1.0", 33
+local MAJOR, MINOR = "LibObjectiveProgress-1.0", 34
 assert(LibStub, MAJOR .. " requires LibStub")
 
 -- Initialize library
