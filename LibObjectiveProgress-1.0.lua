c0bdc14a766d304550f2ac5d07dc46e52f0fdc7f
Caleb Atherton <atcaleb@twitch.tv>
2019-02-27 19:31:23 -0500

    Updated Mythic+ data to account for undocumented Blizzard changes in Patch 8.1.0 (primarily the removal of all % from neutral mobs).

--- LibObjectiveProgress-1.0.lua
+++ LibObjectiveProgress-1.0.lua
@@ -4,7 +4,7 @@ Developed by: Simca@Malfurion (MMOSimca)
 ]]--
 
 -- Set major/minor version
-local MAJOR, MINOR = "LibObjectiveProgress-1.0", 36
+local MAJOR, MINOR = "LibObjectiveProgress-1.0", 39
 assert(LibStub, MAJOR .. " requires LibStub")
 
 -- Initialize library
