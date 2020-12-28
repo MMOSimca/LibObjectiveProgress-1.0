971577376e562901674b71592447671fd8759cc5
MMOSimca <MMOSimca@users.noreply.github.com>
2020-12-28 10:40:04 -0500

    Updated Interface version and readded project revision symbol (hopefully it's fixed by now).

--- LibObjectiveProgress-1.0.lua
+++ LibObjectiveProgress-1.0.lua
@@ -4,7 +4,7 @@ Developed by: Simca@Malfurion (MMOSimca)
 ]]--
 
 -- Set major/minor version
-local MAJOR, MINOR = "LibObjectiveProgress-1.0", 41
+local MAJOR, MINOR = "LibObjectiveProgress-1.0", @project-revision@
 assert(LibStub, MAJOR .. " requires LibStub")
 
 -- Initialize library
